3ae188c4b15e3764adba5262d6223e796835690f
InterLegacy <134627521+InterLegacy@users.noreply.github.com>
2023-10-18 00:13:55 +0700

    Add files via upload

---
+++ web_fitwalk_laravel/resources/resources/views/themes/projectfitwalk/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <title>FitWalk : Official Site</title>
+    @vite([
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+
+        'resources/views/themes/projectfitwalk/assets/css/main.css',
+        'resources/views/themes/projectfitwalk/assets/plugins/jqueryui/jquery-ui.css',
+
+        'resources/views/themes/projectfitwalk/assets/js/main.js',
+        'resources/views/themes/projectfitwalk/assets/plugins/jqueryui/jquery-ui.min.js',
+    ])
+</head>
+<body>
+    @include('themes.projectfitwalk.shared.header')
+    @yield('content')
+    @include('themes.projectfitwalk.shared.footer')
+
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+</html>
---
+++ web_fitwalk_laravel/resources/resources/views/themes/projectfitwalk/shared/footer.blade.php
@@ -0,0 +1,33 @@
+<footer>
+  <div class="container pt-5">
+    <div class="row row-content">
+      <div class="col-md-6">
+        <h1 class="logo-brand">Fit<span>Walk</span></h1>
+        <p>"Logo Disini"</p>
+      </div>
+      <div class="col-md-3 mt-4 mt-sm-0">
+        <h3 class="mb-3">Navigation</h3>
+        <ul class="p-0">
+          <li><a href="#">Home</a></li>
+          <li class="mt-3"><a href="#">Best Seller</a></li>
+          <li class="mt-3"><a href="#">Category</a></li>
+          <li class="mt-3"><a href="#">Comunity</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3 mt-4 mt-sm-0">
+        <h3 class="mb-3">Binus University</h3>
+        <a href="#">JeremyShawn@binus.ac.id</a>
+        <p>Jln. Kemanggisan Raya, No 69 Jakarta Barat Indonesia</p>
+      </div>
+    </div>
+    <div class="row row-copy mt-4 mt-sm-0">
+      <div class="col-md-6">
+        <p>&copy; 2023 FitWalk. All rights reserved.</p>
+      </div>
+      <div class="col-md-6 text-sm-end">
+        <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+        <a href="#"><i class='bx bxl-facebook-circle' ></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
